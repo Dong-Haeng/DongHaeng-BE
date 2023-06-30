e2c705cbf4f2874d948fad56a03f8f41171828e5
민병욱 <woook@minbyeong-ug-ui-MacBookPro.local>
2023-06-30 10:57:36 +0900

    Feat: create member and connect meet and scheduler

--- src/main/java/com/donghaeng/dev/repository/SchedulerRepository.java
+++ src/main/java/com/donghaeng/dev/repository/SchedulerRepository.java
@@ -4,7 +4,10 @@ import com.donghaeng.dev.domain.Scheduler;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface SchedulerRepository extends JpaRepository<Scheduler, Long> {
 
+    List<Scheduler> findByIdentifier(String identifier);
 }
